a0cc1dfea8e373f49df61c7f8c474234f34686d3
mushhadzh <osman.mushhadzh@mail.ru>
2021-04-03 20:54:10 +0400

    Начальная версия программы

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Привет! Как вас зовут?')
+name = input('>>> ')
+print(str(name) + ' выберите товар из списка')
+products = ['1. Шуруповерт', '2. Наушники', '3. Армянский чапалах']
+for x in products:
+	print(x)
+choice = input('Введите цифру нужного вам товара >>> ')
+
+flag = True
+while flag:
+	if int(choice) <= len(products):
+		print('Ваш товар: '+ products[int(choice)-1])
+		flag = False
+	else:
+		print('Товар не найден')
+		flag = False
