2059ca49422233a1ccab483d0ca5f5ee26690c4f
mushhadzh <osman.mushhadzh@mail.ru>
2021-04-03 20:59:16 +0400

    Добавил цены и вывод итоговой суммы

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print('Привет! Как вас зовут?')
 name = input('>>> ')
 print(str(name) + ' выберите товар из списка')
 products = ['1. Шуруповерт', '2. Наушники', '3. Армянский чапалах']
+price = ['3000','23000', '1000000']
 for x in products:
 	print(x)
 choice = input('Введите цифру нужного вам товара >>> ')
@@ -10,6 +11,7 @@ flag = True
 while flag:
 	if int(choice) <= len(products):
 		print('Ваш товар: '+ products[int(choice)-1])
+		print('Сумма к оплате: '+ price[int(choice)-1]+'рублей')
 		flag = False
 	else:
 		print('Товар не найден')
