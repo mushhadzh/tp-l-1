19b63ee4adee3ae0f90d8e52bd90b78b2a49ea68
mushhadzh <osman.mushhadzh@mail.ru>
2021-04-03 22:22:41 +0400

    Добавил возможность пополнять чек и завершить обслуживание

--- main.py
+++ main.py
@@ -5,14 +5,23 @@ products = ['1. Шуруповерт', '2. Наушники', '3. Армянск
 price = ['3000','23000', '1000000']
 for x in products:
 	print(x)
-choice = input('Введите цифру нужного вам товара >>> ')
+
+summ = 0
 
 flag = True
 while flag:
-	if int(choice) <= len(products):
-		print('Ваш товар: '+ products[int(choice)-1])
-		print('Сумма к оплате: '+ price[int(choice)-1]+'рублей')
+	choice = input('Введите цифру нужного вам товара >>> ')
+	if choice == 'exit':
+		print('Сумма к оплате: '+ str(summ)+'рублей')
 		flag = False
+		
+	elif int(choice) <= len(products):
+		summ = summ + int(price[int(choice)-1])
+		print('Ваш товар: '+ products[int(choice)-1])
+		print('Сумма к оплате: '+ str(summ)+'рублей')
+		choice = 0
+		
 	else:
 		print('Товар не найден')
 		flag = False
+
